3b560813f5a75747ae360e862130d65fb47609dc
Rajarshee Chatterjee <cool.rj2008@gmail.com>
2020-06-01 18:37:48 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Route, Switch } from "react-router-dom";
+import Palette from './Palette';
+import PaletteList from './PaletteList';
+import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
+import SingleColorPalette from './SingleColorPalette';
+
+class App extends Component {
+    findPalette(id){
+        return seedColors.find(function(palette){
+            return palette.id === id;
+        })
+    }
+
+    render() {
+        return (
+            <Switch>
+                <Route
+                    exact
+                    path="/"
+                    render={(routeProps) => (
+                        <PaletteList {...routeProps} palettes={seedColors} />
+                    )}
+                />
+                <Route
+                    exact
+                    path="/palette/:id"
+                    render={routeProps => (
+                        <Palette
+                            palette={generatePalette(
+                                this.findPalette(routeProps.match.params.id)
+                            )}
+                        />
+                    )}
+                />
+                <Route
+                    exact
+                    path='/palette/:paletteId/:colorId'
+                    render={routeProps => (
+                        <SingleColorPalette
+                            colorId={routeProps.match.params.colorId}
+                            palette={generatePalette(
+                                this.findPalette(routeProps.match.params.paletteId)
+                            )}
+                        />
+                    )}
+                />
+            </Switch>
+            // <div>
+            //     <Palette palette={generatePalette(seedColors[4])} />
+            // </div>
+        )
+    }
+}
+
+export default App;
