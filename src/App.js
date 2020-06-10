3689d7013a0a592a61133127df061e802b159faa
Rajarshee Chatterjee <cool.rj2008@gmail.com>
2020-06-10 09:05:03 +0530

    Add New Palette Form & Link

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 import SingleColorPalette from './SingleColorPalette';
+import NewPaletteForm from './NewPaletteForm';
 
 class App extends Component {
     findPalette(id){
         return seedColors.find(function(palette){
             return palette.id === id;
-        })
+        });
     }
 
     render() {
         return (
             <Switch>
+                <Route
+                    exact
+                    path="/palette/new"
+                    render={() => (
+                        <NewPaletteForm />
+                    )}
+                />
                 <Route
                     exact
                     path="/"
@@ -47,9 +55,6 @@ class App extends Component {
                     )}
                 />
             </Switch>
-            // <div>
-            //     <Palette palette={generatePalette(seedColors[4])} />
-            // </div>
         )
     }
 }
---
+++ src/NewPaletteForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewPaletteFOrm extends Component {
+    render() {
+        return (
+            <div>
+                <h1>New Palette Form XD</h1>
+            </div>
+        )
+    }
+}
+
+export default NewPaletteFOrm;
